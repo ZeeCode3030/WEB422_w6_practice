35f364838ab17cd9d696e2e57bbaa295001cc978
Hephzibah Empress Nick-Oshomah <120137330+ZeeCode3030@users.noreply.github.com>
2023-10-19 21:15:39 +0000

    committing something

---
+++ my-app/pages/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Footer(){
+
+    return (
+        <>
+        <h3>
+            THIS IS THE FOOTER
+        </h3>
+        </>
+    )
+}
--- my-app/pages/Layout.jsx
+++ my-app/pages/Layout.jsx
@@ -1,8 +1,13 @@
-
+import Nav from 'Nav'
+import Footer from './Footer'
 
 export default function Layout({props}){
 
     return(
-        
+        <>
+        <Nav/>
+        {props.children}
+        <Footer/>
+        </>
     )
 }
