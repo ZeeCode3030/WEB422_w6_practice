1c21442ded090f0c4845e65ed73072a4109f5cb5
Hephzibah Empress Nick-Oshomah <120137330+ZeeCode3030@users.noreply.github.com>
2023-10-19 14:36:08 +0000

    committing first version of app

---
+++ my-app/pages/nav.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Nav(){
+
+    return (
+        <>
+        <p>
+        <Link href={'/'}>HOME</Link> | 
+        <Link href={'/about.js'}>ABOUT</Link> | <Link href={'/dashboard'}> DASHBOARD </Link>
+        </p>
+       </>
+    )
+
+}
